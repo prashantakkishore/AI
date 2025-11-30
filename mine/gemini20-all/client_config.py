f0cc5a755e2f94d15e7cd031a465cbe9e9b5416d
Prashantak Kishore Srivastava <prashantak.kishore@gmail.com>
2025-11-30 17:58:48 -0500

    basics llms

--- mine/gemini20-all/client_config.py
+++ mine/gemini20-all/client_config.py
@@ -2,7 +2,6 @@
 
 import os
 from google import genai
-from google.genai.types import HttpOptions
 from dotenv import load_dotenv
 
 # Load environment variables (ensure this is called before client is created)
@@ -10,11 +9,9 @@ load_dotenv()
 
 # The genai.Client object
 # This code runs only ONCE when the module is first imported.
-client = genai.Client(
-    http_options=HttpOptions(api_version="v1")
-)
+client = genai.Client()
 
-MODEL = "gemini-2.5-flash-live-api" 
-TRANSCRIPTION_MODEL = "gemini-1.5-flash"
-# MODEL = "gemini-2.0-flash-exp"
-# TRANSCRIPTION_MODEL = "gemini-1.5-flash-8b"
+# MODEL = "gemini-1.5-flash" 
+# TRANSCRIPTION_MODEL = "gemini-1.5-flash"
+MODEL = "gemini-2.0-flash-exp"
+TRANSCRIPTION_MODEL = "gemini-1.5-flash-8b"
